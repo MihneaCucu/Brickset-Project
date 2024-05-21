c6ece802ae49efc33566328071c1dd0284999041
Mihnea Cucu <68858101+MihneaCucu@users.noreply.github.com>
2024-05-21 19:29:24 +0300

    Update limited_set_lego.cpp

--- Brickset/limited_set_lego.cpp
+++ Brickset/limited_set_lego.cpp
@@ -1,20 +1,17 @@
-#include "limited_set_lego.h"
-#include <iostream>
+#ifndef limited_set_lego_h
+#define limited_set_lego_h
+#include "set_lego.h"
 
-limited_set_lego::limited_set_lego(int id, std::string nume, int an_lansare, double pret_lansare, int interval)
-    : set_lego(id, nume, an_lansare, pret_lansare), interval(interval) {}
+class limited_set_lego : virtual public set_lego {
+private:
+    int interval;
 
-int limited_set_lego::get_interval() const {
-    return interval;
-}
+public:
+    limited_set_lego(int id, std::string nume, int an_lansare, double pret_lansare, int interval);
+    int get_interval() const;
+    void set_interval(int interval);
+    ~limited_set_lego() override;
+};
 
-void limited_set_lego::set_interval(int interval) {
-    this->interval = interval;
-}
-
-limited_set_lego::~limited_set_lego() {
-    /*std::cout << "Destructor called for limited_set_lego with time_limit: " << interval << std::endl;*/
-    piese.clear();
-}
-
+#endif
 
